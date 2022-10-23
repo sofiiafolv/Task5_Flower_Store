ea8afb2df2071a6fe76e087e85224b3f89cac8f5
Sofiia Folvarochna <sofiyafolv@gmail.com>
2022-10-23 15:20:02 +0300

    Added new files

---
+++ src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,18 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    private List<FlowerPack> flbucket = new ArrayList<FlowerPack>();
+    public void add(FlowerPack flpack){
+        flbucket.add(flpack);
+    }
+    public double getPrice(){
+        double sum = 0;
+        for(int i = 0; i < flbucket.size(); ++i){
+            sum += flbucket.get(i).getPrice();
+        }
+        return sum;
+    }
+}
